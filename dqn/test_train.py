c4f9af2057984f27e788060d664aaf40937f9b87
Jona Schulz <schulzj@student.ethz.ch>
2023-12-22 15:39:32 +0100

    trained dqn agents

--- dqn/test_train.py
+++
@@ -1,9 +0,0 @@
-from agent import Agent
-import os
-
-env_ids = ['MinAtar/Asterix-v0', 'MinAtar/Breakout-v0', 'MinAtar/Seaquest-v0', 'MinAtar/Freeway-v0']
-model_dirs = ['asterix', 'breakout', 'seaquest', 'freeway']
-
-for i, _id in enumerate(env_ids):
-    agent = Agent(_id)
-    agent.train(4000, save_interval=None, save_path=os.path.join(model_dirs[i], 'model.pt'))
---
+++ dqn/train_agents.py
@@ -0,0 +1,17 @@
+from agent import Agent
+import os
+import torch
+
+env_ids = ['MinAtar/Asterix-v0', 'MinAtar/Breakout-v0', 'MinAtar/Seaquest-v0', 'MinAtar/Freeway-v0']
+model_dirs = ['asterix', 'breakout', 'seaquest', 'freeway']
+num_steps = [1000000, 10000000]
+
+agent = Agent(env_ids[3])
+agent.policy_net.load_state_dict(torch.load("freeway/model_100000.pt", map_location="cpu"))
+agent.play(50)
+exit()
+
+for i, _id in enumerate(env_ids):
+    agent = Agent(_id)
+    for num_step in num_steps:
+        agent.train(num_step, save_interval=None, save_path=os.path.join(model_dirs[i], f'model_{num_step}.pt'))
